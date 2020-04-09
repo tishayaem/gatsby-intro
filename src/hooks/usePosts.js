5393954a8189fc39285c3bd4a2a410b08aaa46a0
tishayaem <constant.kalinin@gmail.com>
2020-04-09 23:16:08 +0100

    Render Mdx blog posts

---
+++ src/hooks/usePosts.js
@@ -0,0 +1,23 @@
+import { graphql, useStaticQuery } from 'gatsby';
+const usePosts = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            title
+            slug
+            author
+          }
+        }
+      }
+    }
+  `);
+  return data.allMdx.nodes.map(post => ({
+    title: post.frontmatter.title,
+    author: post.frontmatter.author,
+    slug: post.frontmatter.slug,
+    excerpt: post.excerpt,
+  }));
+};
+export default usePosts;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <Link to="/about/">About Me </Link>
-  </Layout>
-);
+import usePosts from '../hooks/usePosts';
+export default () => {
+  const posts = usePosts();
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <Link to="/about/">About Me </Link>
+
+      <h2>My blog</h2>
+      {posts.map(post => (
+        <pre>{JSON.stringify(post, null, 2)}</pre>
+      ))}
+    </Layout>
+  );
+};
